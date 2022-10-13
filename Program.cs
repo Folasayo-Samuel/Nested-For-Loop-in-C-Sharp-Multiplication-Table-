dbfb3569048dcda801fd872fc375c60e14094113
Folasayo Samuel <folasayosamuel@gmail.com>
2022-10-13 11:06:41 +0100

    update

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+	class Program
+	{
+		static void Main(string[]args)
+		{
+			for(int j = 0; j <= 10; j++)
+			{
+				for(int i = 1; i <= 10; i++)
+				{
+					Console.WriteLine($"{i}*{j} = {i * j}\t");
+				}
+				Console.WriteLine();
+
+			}
+            Console.ReadKey();
+        }
+	}
+}
